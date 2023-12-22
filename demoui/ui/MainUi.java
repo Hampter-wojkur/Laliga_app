28ff7ae902b77e7923554b6f6b4ed906817ef280
wmarcela <wmarcela@student.agh.edu.pl>
2023-12-22 17:38:21 +0100

    feat: emergency commit XD

--- demoui/ui/MainUi.java
+++ demoui/ui/MainUi.java
@@ -1,19 +1,35 @@
 package demoui.ui;
 
+import demoui.SignGUI;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class MainUi {
     private JPanel jp;
+
+    private final String LaLigaID = "140";
+    private final String PremierLeagueID = "39";
+    private final String SeriaAID = "135";
+    private final String BundesligaID = "78";
+    private final String Ligue1ID = "61";
     private JTable table;
     private JComboBox leagueCombo;
     private JButton logOutButton;
 
     public MainUi(){
         createTable();
+        createComboBox();
+        performLogOutButton();
+        comboBoxClicked();
     }
     public JPanel getRootPanel(){
         return jp;
@@ -41,5 +57,51 @@ public class MainUi {
         for (int i = 2; i < 9; i++){
             columns.getColumn(i).setCellRenderer(centerRender);
         }
+    }
+
+    private void createComboBox(){
+        String[] leagues = { "LaLiga", "Premier League", "Bundesliga", "Serie A", "Ligue 1" };
+        leagueCombo.setModel(new DefaultComboBoxModel(leagues));
+
+    }
+
+
+    private void performLogOutButton(){
+        logOutButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComponent comp = (JComponent) e.getSource();
+                Window win = SwingUtilities.getWindowAncestor(comp);
+                win.dispose();
+                SignGUI signGUI = new SignGUI();
+                signGUI.createGUI();
+            }
+        });
+    }
+
+    private void comboBoxClicked(){
+        leagueCombo.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String leagueId = (String) leagueCombo.getSelectedItem();
+
+                    switch (leagueId){
+                        case LaLigaID:
+                            break;
+                        case PremierLeagueID:
+                            break;
+                        case SeriaAID:
+                            break;
+                        case BundesligaID:
+                            break;
+                        case Ligue1ID:
+                            break;
+                        default:
+                    }
+                }
+            }
+        });
     }
 }
--- demoui/ui/SignUpUI.java
+++ demoui/ui/SignUpUI.java
@@ -10,4 +10,11 @@ public class SignUpUI {
     private JLabel signLabel;
     private JPanel dataPanel;
     private JPanel labelPanel;
+    private JLabel usernameLabel;
+    private JLabel repeatPasswordLabel;
+    private JLabel passworddLabel;
+
+    public JPanel getRootPanel(){
+        return jp;
+    }
 }
